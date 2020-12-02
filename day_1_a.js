44f4bada3eb3b89aba704a67ad7be87b98b9b5d5
cjb230 <james.barton@blueyonder.co.uk>
2020-12-03 00:11:16 +0100

    Added a ms timer.

--- day_1_a.js
+++ day_1_a.js
@@ -1,4 +1,6 @@
 // https://adventofcode.com/2020/day/1
+const startTime = process.hrtime()
+
 fs = require('fs')
 fs.readFile('day_1_input', 'utf8', function(err,data) {
     if (err) {
@@ -15,4 +17,9 @@ fs.readFile('day_1_input', 'utf8', function(err,data) {
             }
         }
     }
+    return
 })
+
+const endTime = process.hrtime()
+const ms_elapsed = (1000 * (endTime[0] - startTime[0])) + (endTime[1] - startTime[1]) / 1000000
+console.log('ms elapsed = ' + ms_elapsed)
